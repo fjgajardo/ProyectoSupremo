28d6927f372c4c6eb5e7c42554ae4013b07141ea
Fernando Gajardo <fjgajardo@uc.cl>
2021-02-10 04:02:52 -0300

    CommandPatternInicio

---
+++ Assets/Scripts/Commands/AttackCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCommand : ICommand
+{
+    private Rigidbody2D body;
+
+    public AttackCommand(Rigidbody2D body){
+
+        this.body = body;
+    }
+
+    public void Execute()
+    {  
+        Debug.Log("Attack");
+
+    }
+    public void Execute2()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour 
+{
+    public Rigidbody2D body;
+    private ICommand buttonSpace;
+    private ICommand buttonE;
+    private ICommand buttonQ;
+
+    Vector2 movement;
+    public float speed = 5f;
+
+
+    void Start() {
+        buttonSpace = new AttackCommand(body);
+        buttonE = new SpecialAttackCommand(body);
+        buttonQ = new AttackCommand(body);
+
+
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical"); 
+        if (Input.GetKey(KeyCode.E)&& (Input.GetKey(KeyCode.Space))){
+            buttonE.Execute();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            buttonSpace.Execute();
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            buttonQ.Execute();
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            buttonE.Execute2();
+        }
+        else if (Input.GetKeyDown(KeyCode.E)&& (Input.GetKeyDown(KeyCode.Space))){
+            buttonE.Execute();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        body.MovePosition(body.position + movement * speed * Time.fixedDeltaTime);
+    }
+}

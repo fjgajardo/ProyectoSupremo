4eecff4325cf428ffaf576b1e30fc5584e67f296
Fernando <fjgajardo@uc.cl>
2021-02-15 18:48:22 -0300

    betterDash

--- Assets/Scripts/Commands/DashCommand.cs
+++ Assets/Scripts/Commands/DashCommand.cs
@@ -5,36 +5,29 @@ using UnityEngine;
 public class DashCommand : ICommand
 {
     private Rigidbody2D body;
-    private float time;
-    private Vector2 direction;
-    
+    private Vector2 direction;    
     private float dashSpeed;
-    private float startDashTime = 0.1f;
-    private float dashTime;
-    private IEnumerator coroutine;
+    private float dashSpeedDecrease;
+    private InputManager input;
 
-    public DashCommand(Rigidbody2D body, float time, Vector2 direction){
+    public DashCommand(Rigidbody2D body, Vector2 direction, InputManager input){
         this.body = body;
-        this.time = time;
         this.direction = direction;
-        this.dashTime = startDashTime;
-        dashSpeed = 50f;
+        this.input = input;
     }
 
     public void Execute()
     {  
-        //Prueba esto con drag, los valores que importan son StartDashTime y dashSpeed
-        while (dashTime > 0)
+        dashSpeed = 200f;
+        dashSpeedDecrease = 0.1f;
+        float dashSpeedMin = 100f;
+        while (dashSpeed >= dashSpeedMin)
         {
-            if (body.velocity == Vector2.zero)
-            {
-                body.AddForce(direction*dashSpeed);
-            }
-            dashTime -= Time.deltaTime;
-        } 
-        dashTime = startDashTime;
-        body.velocity = Vector2.zero;
-        
+            dashSpeed -= dashSpeedDecrease * dashSpeed;
+            body.velocity = direction.normalized * dashSpeed;
+            Debug.Log(dashSpeed + "   " + body.velocity);
+        }
+        input.state = InputManager.States.Normal;
 
     }
 
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,49 +8,94 @@ public class InputManager : MonoBehaviour
     private ICommand buttonSpace;
     private ICommand buttonE;
     private ICommand buttonQ;
-
+    public enum States
+    {
+        Normal, 
+        Dashing,   
+    }
     Vector2 direction;
-    public float speed = 3f;
+    public float speed;
     float time;
+    public States state;
     
 
 
-    void Start() {
-        buttonSpace = new DashCommand(body, time, direction);
+    void Awake() {
         buttonE = new AttackCommand(body);
         buttonQ = new AttackCommand(body);
+        speed = 8f;
+        state = States.Normal;
     }
 
     void Update()
     {
+        switch (state)
+        {
+            case States.Normal:
+                direction.x = 0f;
+                direction.y = 0f;
+
+                if (Input.GetKey(KeyCode.W))
+                {
+                    direction.y = +1f;
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    direction.y = -1f;
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    direction.x = -1f;
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    direction.x = +1f;
+                }
+
+                time = Time.deltaTime;
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    //Me di cuenta que basta con crearlo dentro del if para mandar el time dentro de la creación del comando
+                    //Busque y es comun hacerlo así, no es negro
+                    buttonSpace = new DashCommand(body, direction, this);
+                    state = States.Dashing;
+                    
+                }
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    buttonQ.Execute();
+                }
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    buttonE.Execute();
+                }
+                if (Input.GetKeyDown(KeyCode.E)&& (Input.GetKeyDown(KeyCode.Space))){
+                    buttonE.Execute();
+                }
+                break;
+            case States.Dashing:
+                break;
+        }
         
-        time = Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //Me di cuenta que basta con crearlo dentro del if para mandar el time dentro de la creación del comando
-            //Busque y es comun hacerlo así, no es negro
-            time = Time.deltaTime;
-            buttonSpace = new DashCommand(body, time, direction);
-            buttonSpace.Execute();
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            buttonQ.Execute();
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            buttonE.Execute();
-        }
-        if (Input.GetKeyDown(KeyCode.E)&& (Input.GetKeyDown(KeyCode.Space))){
-            buttonE.Execute();
-            
-        }
-        direction.x = Input.GetAxisRaw("Horizontal");
-        direction.y = Input.GetAxisRaw("Vertical"); 
+
+
     }
 
     void FixedUpdate()
     {
-        body.MovePosition(body.position + direction * speed * Time.fixedDeltaTime);
+        switch (state)
+        {
+            case States.Normal:
+                direction = direction.normalized * speed;
+                body.velocity = direction;
+                break;
+
+            case States.Dashing:
+                buttonSpace.Execute();
+                break;
+
+        }
+        
     }
 }
